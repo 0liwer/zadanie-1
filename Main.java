b7dccc0ac5e8e96af1f3360e871e02569111e752
0liwer <oliwer.suwik@gmail.com>
2024-10-12 18:19:07 +0200

    zadanie w domu napraw

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int inputNumber = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Podana liczba to: " + inputNumber);
+    }
+}
